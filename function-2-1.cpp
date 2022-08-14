71423798fec12352ec525eb20fdd07dd6a76351c
Max Bruce <maxbruce@Maxs-MacBook-Air-2.local>
2022-08-14 22:21:53 +0930

    cheer

---
+++ function-2-1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+float add_op(float left, float right);
+float subtract_op(float left, float right);
+float arithmetic_ops(float left, float right, std::string op);
+
---
+++ function-2-4.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+float multiply_op(float left, float right);
